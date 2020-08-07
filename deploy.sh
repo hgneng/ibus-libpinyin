09dcffb9c02d8f3ad25065beb098dfb370084e99
Cameron Wong <hgneng@gmail.com>
2020-08-07 10:46:13 +0800

    fix install issue

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
 sudo apt install -y libsqlite3-dev
+sudo apt install -y sqlite3
 sudo apt install -y sqlite3-dev
+sudo apt install -y libsqlite3-dev
+sudo apt install -y libibus-1.0-dev
 sudo apt install -y libpinyin-dev
+sudo apt install -y gnome-common
 
 ./autogen.sh
 ./configure  --prefix=/usr --libexecdir=/usr/lib/ibus
