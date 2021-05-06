f259504d0abec9bb63f1da1c99e2db2b773dd0c8
Cameron Wong <hgneng@gmail.com>
2021-05-06 13:49:12 +0800

    fix char_limit

--- src/ibusspeech.cc
+++ src/ibusspeech.cc
@@ -9,7 +9,8 @@ using namespace std;
 extern int ibs_word_begin;
 extern int ibs_word_end;
 extern char* ibs_words[];
-extern int ibs_char_limit;
+int ibs_char_limit;
+bool g_is_mute = true;
 
 static SPDConnection* g_spd = 0;
 static char g_result[1024];
@@ -19,40 +20,61 @@ void ibs_destroy() {
     spd_close(g_spd);
 }
 
-void ibs_speak(char *text) {
+void ibs_speak(const char *text) {
   g_message("ibs_speak: %s", text);
+  if (g_is_mute) {
+    return;
+  }
   //spd_cancel(g_spd);
   int ret = spd_say(g_spd, SPD_TEXT, text);
   //g_message("ibs_speak: ret=%d", ret);
 }
 
-extern "C"
-void ibs_explain(char *text) {
-  g_message("ibs_explain: %s", text);
-
-  if (strlen(text) <= 3) {
-    string s(text);
-    int code = utf8::peek_next(s.begin(), s.end());
-    g_message("ibs_explain: %d", code);
-    if (code < ibs_word_end && code > 0 && ibs_words[code] > 0) {
-      ibs_speak(ibs_words[code]);
-    } else {
-      ibs_speak(text);
-    }
-  } else {
-    ibs_speak(text);
-  }
-}
-
-void ibs_speak_politely(char *text) {
+void ibs_speak_politely(const char *text) {
   g_message("ibs_speak_politely:%s", text);
+  if (g_is_mute) {
+    return;
+  }
   int ret = spd_say(g_spd, SPD_MESSAGE, text);
   //g_message("ibs_speak: ret=%d", ret);
 }
 
-void ibs_stop() {
-  //printf("ibs_stop\n");
-  spd_cancel(g_spd);
+string ibs_get_utf8(int code) {
+  char buf[5] = {0, 0, 0, 0, 0};
+  try {
+    utf8::append(code, buf);
+  } catch (...) {
+    //cerr << "code point:" << code << endl;
+  }
+  return string(buf);
+};
+
+extern "C"
+void ibs_explain(char *text) {
+  g_message("ibs_explain: %s", text);
+
+  if (strlen(text) / 3 <= ibs_char_limit) {
+    string s(text);
+    string result;
+    std::string::iterator it = s.begin();
+    int code = 0;
+    bool first = true;
+    const char *c = 0;
+    while (it != s.end()) {
+      code = utf8::next(it, s.end());
+      g_message("ibs_explain: %d", code);
+      if (code < ibs_word_end && code > 0 && ibs_words[code] > 0) {
+        c = ibs_words[code];
+      } else {
+        c = ibs_get_utf8(code).c_str();
+      }
+
+      result.append(c);
+    }
+    ibs_speak(result.c_str());
+  } else {
+    ibs_speak(text);
+  }
 }
 
 void ibs_exec(const char *cmd) {
@@ -72,14 +94,20 @@ void ibs_exec(const char *cmd) {
   pclose(fp);
 }
 
-int ibs_is_proc_running(char *cmd) {
+bool ibs_is_proc_running(const char *cmd) {
   char buffer[1024];
   snprintf(buffer, 1023, "ps -ef|grep '%s'|grep -v grep", cmd);
   ibs_exec(buffer);
   if (strlen(g_result) > 0)
-    return 1;
+    return true;
   else
-    return 0;
+    return false;
+}
+
+void ibs_stop() {
+  //printf("ibs_stop\n");
+  spd_cancel(g_spd);
+  g_is_mute = !ibs_is_proc_running("orca");
 }
 
 /* rate in Orac is 0-100, default is 50 */
@@ -115,10 +143,7 @@ void ibs_update_rate() {
   g_message("ibs_update_rate: %d -> %d", rate, (int)((rate - 50) * 2));
 }
 
-
 void ibs_update_char_limit() {
-  return; // undefined reference to `ibs_char_limit`
-
   ibs_exec("grep char_limit ~/.dog/config/ibusreader | awk -F= '{print $2}' 2>/dev/null");
   ibs_char_limit = 2;
   if (strlen(g_result) > 0) {
@@ -138,8 +163,11 @@ void ibs_init() {
     g_spd = spd_open2("IBusSpeech", "main", NULL, SPD_MODE_THREADED, NULL, 1, &error_result);
     if (g_spd) {
       ibs_update_rate();
-      //ibs_update_char_limit();
-      spd_say(g_spd, SPD_TEXT, "i-bus reader 已启动");
+      ibs_update_char_limit();
+      if (ibs_is_proc_running("orca")) {
+        g_is_mute = false;
+        spd_say(g_spd, SPD_TEXT, "i-bus reader 已启动");
+      }
     } else {
       g_message("fail to init speechd: %s", error_result);
     }
