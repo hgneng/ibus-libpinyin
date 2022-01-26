e0eaa3204872032d5c41cbb194a9132134a3986c
Cameron Wong <hgneng@gmail.com>
2022-01-26 08:27:31 +0800

    update dependency

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 sudo apt install -y libsqlite3-dev
 sudo apt install -y sqlite3
-sudo apt install -y sqlite3-dev
 sudo apt install -y libsqlite3-dev
 sudo apt install -y libibus-1.0-dev
 sudo apt install -y libpinyin-dev
 sudo apt install -y gnome-common
+sudo apt install -y libspeechd-dev
 
 ./autogen.sh
 ./configure  --prefix=/usr --libexecdir=/usr/lib/ibus
