c7ed11f5b34d6353c4ae5ef5e0f939183574082c
Cameron Wong <hgneng@gmail.com>
2024-04-12 10:14:43 +0800

    add dependency

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 sudo apt install -y build-essential autoconf automake libtool
 sudo apt install -y libsqlite3-dev sqlite3 libibus-1.0-dev libpinyin-dev gnome-common libspeechd-dev ibus-libpinyin
+sudo apt install -y gobject-introspection
 
 ./autogen.sh
 ./configure  --prefix=/usr --libexecdir=/usr/lib/ibus
